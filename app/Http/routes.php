6fac9a3a34819c28d133260c0dd9e0cc4926dfb2
Phil Weier <pweier@my.bcit.ca>
2016-04-20 16:19:53 -0700

    fixed route to show users

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/testdb', function(){
-    return User::all();
+    return (DB::table('users')->get());
 });
 
 Route::auth();
