cc2590a27a4ac9c9d92a9a0c76bac9fb5d8f6084
jackmtr <jackmtr@hotmail.com>
2016-04-23 16:08:42 -0700

    styled the events and login page

--- resources/views/eventFolder/events.blade.php
+++ resources/views/eventFolder/events.blade.php
@@ -1,50 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-    	<a href="{{ url('/events/create') }}"> (icon) Add Event</a>
-        <h2>Active Events</h2>
-        
-		<table class="table-wrapper">
-		<tr>
-			<th>Event</th>
-			<th>Status</th>
-			<th>Date</th>
-			<th>Time</th>
-			<th>Location</th>
-			<th>Description</th>
-			<th># of Tables</th>
-			<th># Seats per Table</th>
-		</tr>
-		@if (count($openEvents))
-			@foreach($openEvents as $openEvent)
-				<tr>
-				<td>{{ $openEvent['event_name'] }}</td>
-				<td>
-					@if ( $openEvent['event_status'] == 0)
-					OPEN
-					@endif
-				</td>
-				<td>{{$openEvent['event_date']}}</td>
-				<td>{{$openEvent['event_time']}}</td>
-				<td>{{$openEvent['event_location']}}</td>
-				<td>{{$openEvent['event_description']}}</td>
-				<td>{{$openEvent['num_of_tables']}}</td>
-				<td>{{$openEvent['seats_per_table']}}</td>
-				</tr>
-
-			@endforeach
-		@else
-				<p>No Contacts Exist</p>
-		@endif
-		</table>
-
-    </div>
-    <div>
-        <h2>Ended Events</h2>
-        <table>
-		<tr><th>Event</th><th>Status</th><th>Date</th><th>Time</th><th>Location</th><th>Description</th><th># of Tables</th><th># Seats per Table</th></tr>        	
-        	
-        </table>
+	<div class="events">
+	    <div class="topevents">
+	    	<div class="topeventnav">
+		    	<h2>Active Events</h2>
+		        <a href="{{ url('/events/create') }}">Add Event</a>
+	    	</div>
+	    	<div class="openevents">
+		    	@foreach($openEvents as $openEvent)
+		    		<div class="singleopenevent">
+		    			<div class="singleeventleftbox">
+		    				<p>{{$openEvent['event_date']}}</p>
+		    				<p>{{$openEvent['event_name']}}</p>
+		    				<p>{{$openEvent['event_location']}}</p>
+		    			</div>
+		    			<div class="singleeventrightbox">
+			    			<div class="checkedin">
+			    				<p>120</p>
+			    				<p>Checked In</p>			    				
+			    			</div>
+			    			<div class="duplicate">
+			    				<a href="#">Duplicate</a>
+			    			</div>
+		    			</div>
+		    		</div>
+		    	@endforeach
+	    	</div>
+	    </div>
+	    <div class="completedevents">
+	        <h2>Ended Events</h2>
+	    </div>
     </div>
 @endsection
