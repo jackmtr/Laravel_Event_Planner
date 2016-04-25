542fbb24fa1023b80f23f36f01476c03315b365e
hasssanhosseinpour <hassanhosseinpoor@yahoo.com>
2016-04-25 15:53:10 -0700

    Update event with Jackie.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,7 +6,7 @@ use App\Event;
 use App\Http\Requests;
 use App\EventWithCount;
 //use Carbon\Carbon;
-use Request;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
@@ -54,6 +54,36 @@ class EventController extends Controller
         $event = Event::find($id);
 
         return view('eventFolder.editEvents', compact("event"));
+    }
+
+    public function update(Request $request, $id)
+    {
+        //Validating data
+        $this->validate($request, [
+            'event_name' => 'required|max:255',
+            'event_date' => '',
+            'num_of_tables' => 'integer',
+            'seats_per_table' => 'integer',
+
+        ]);
+                       
+        //Save data to database
+        $event = Event::find($id);
+        //dd($request->input('event_name'));
+        
+        $event->event_name = $request->input('event_name');
+        $event->event_date = $request->input('event_date');
+        $event->event_time = $request->input('event_time');
+        $event->event_location = $request->input('event_location');
+        $event->event_description = $request->input('event_description');
+        $event->num_of_tables = $request->input('num_of_tables');
+        $event->seats_per_table = $request->input('seats_per_table');
+
+        $event->save();
+        return redirect('events');
+               
+        //return redirect()->route('posts.show', $post->id);
+
     }
 
 }
