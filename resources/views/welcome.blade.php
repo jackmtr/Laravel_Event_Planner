2a348017e64953fbe52d04158c1e601ce0d72e3e
Phil Weier <pweier@my.bcit.ca>
2016-04-29 14:04:16 -0700

    update heroku master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
       <div class="panel panel-default">
-        <div class="panel-heading">Project Outline - Last Updated: 3:04pm April 28,2016</div>
+        <div class="panel-heading">Project Outline - Last Updated: 2:04pm April 29,2016</div>
 
         <div class="panel-body">
           <h3>Team Members</h3>
