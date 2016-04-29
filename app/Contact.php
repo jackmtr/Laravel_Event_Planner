ce93b0b4a537fea6fd1672be4e0f07aa1d18db06
jackmtr <jackmtr@hotmail.com>
2016-04-28 23:23:26 -0700

    added the model relationships to the models - table seating tables

--- app/Contact.php
+++ app/Contact.php
@@ -11,18 +11,45 @@ class Contact extends Model
 
 	protected $primaryKey = "contact_id";
 
-    protected $fillable = [
-    	'first_name',
-    	'last_name',
+	protected $fillable = [
+		'first_name',
+		'last_name',
 		'email',
 		'occupation',
 		'company',
 		'wechat_id',
 		'notes',
-		'added_by',
-    ];
+		'added_by',//should eventually remove, should't be editable
+	];
 
-    protected $dates = ["deleted_at"];
+	protected $dates = ["deleted_at"];
 
-    public $timestamps = false;
+	public $timestamps = false;//might remove to allow timstamps
+
+	/**
+	* A contact may be on many guestlists.
+	*
+	* @return \Illuminate\Database\Eloquent\Relations\HasMany
+	*/
+	public function guestList(){
+		return $this->hasMany('App\GuestList');
+	}	
+
+	/**
+	* A contact may have many phonenumbers.
+	*
+	* @return \Illuminate\Database\Eloquent\Relations\HasMany
+	*/
+	public function phoneNumbers(){
+		return $this->hasMany('App\PhoneNumber');
+	}
+
+    /**
+    * A contact is added by a user.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }	    	  
 }
