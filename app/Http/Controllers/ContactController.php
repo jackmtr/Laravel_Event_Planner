843f4db74f05850deebd13d663d438ac5fa8d11e
Phil Weier <pweier@my.bcit.ca>
2016-04-29 14:12:55 -0700

    testing use App/GuestList EventController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Contact;
 use App\Event;
+use App\GuestList;
 use App\Http\Requests\ContactRequest;
 use Request; //needed for the search function atm
 use Auth;
