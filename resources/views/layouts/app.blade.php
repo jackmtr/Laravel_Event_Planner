7ff68adcda6805164fe1034bf04495b028b28e28
jackmtr <jackmtr@hotmail.com>
2016-04-30 14:23:30 -0700

    stripped down the scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,67 +2,40 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Istuary Event CRM</title>
-
-    <!-- Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Rajdhani:600' rel='stylesheet' type='text/css'>
-    <!--<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>-->
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-
-    <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="/css/framework.css"/>
-    <!--{{-- <link href="{{ elixir('css/framework.css') }}" rel="stylesheet"> --}}-->
-
-    <!--<style>
-        body {
-            font-family: 'Lato';
-        }
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>-->
-
+    <link href='https://fonts.googleapis.com/css?family=Rajdhani:600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">    
 </head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top clearfix">
+
+<body><!--id="app-layout" -->
+
+    <nav class="navbar"> <!-- navbar-default navbar-static-top clearfix-->
         <div class="container">
-            <div class="navbar-header navbar-left">
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+
+            <div> <!--class="navbar-header navbar-left"-->
+                <a class="navbar-logo" href="{{ url('/') }}"> <!--class="navbar-brand" -->
                     Istuary Event Management & CRM
                 </a>
             </div>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-
+            <div> <!-- class="collapse navbar-collapse" id="app-navbar-collapse"-->
                 <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    <!-- Loging code here is irrelevent because you should always be redirected out if not loged in-->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                    @else
-                        <li class="dropdown">
+                <ul class="navbar-menu"> <!--class="nav navbar-nav navbar-right"-->
+                        <li> <!--class="dropdown"-->
                             <p>
                                 Active users: 1
                             </p>
 
                             <a href="{{ url('/logout') }}" role="button" aria-expanded="false">Logout {{ Auth::user()->name }}</a>
                         </li>
-
-                    @endif
                 </ul>
             </div>
         </div>
 
         <!-- Left Side Of Navbar -->
-        <div class="subnav">
-            @if(!Auth::guest())
-            <ul class="second-navbar">
+        <div> <!--class="subnav"-->
+            <ul class="navbar-menu"> <!-- class="second-navbar"-->
                 <li>
                     <a href="{{ url('/events/') }}">EVENTS</a>
                 </li>
@@ -70,7 +43,6 @@
                     <a href="{{ url('/contacts') }}">CONTACTS</a>
                 </li>
             </ul>
-            @endif
         </div>
     </nav>
 
