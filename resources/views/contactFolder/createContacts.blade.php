984eeb0e4c5aa813e04e304a9739634a36dd76dd
jackmtr <jackmtr@hotmail.com>
2016-05-02 16:34:52 -0700

    dabbling with buttons

--- resources/views/contactFolder/createContacts.blade.php
+++ resources/views/contactFolder/createContacts.blade.php
@@ -11,4 +11,69 @@
 
     @include('errors._list')
 </div>
+
+<div ng-app="">
+	<div class="sg-spacer-xx-lg">
+		<button type="button" name="button" class="button-default" ng-click="popup1=true">Popup with Confirmation</button>
+		<div class="popup ng-hide" style="display: block;" ng-show="popup1">
+			<div class="popup-mask">
+				<div class="panel">
+					<div class="panel-inner">
+						<div class="popup-cancel">
+							<a href="#" ng-click="popup1=false;"><i class="fa fa-fw fa-times"></i></a>
+						</div>
+						<h2>Thank you for your feedback!</h2>
+						<p>
+							You’ll be the first to know once the new update arrives. How exciting!
+						</p>
+						<p class="popup-button">
+							<button type="button" name="button" class="button-primary" ng-click="popup1=false;">Cool. Take me back to home page</button>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<button type="button" name="button" class="button-default" ng-click="popup2 = true;">Popup without Close</button>
+		<div class="popup ng-hide" style="display: block;" ng-show="popup2">
+			<div class="popup-mask">
+				<div class="panel">
+					<div class="panel-inner">
+						<h2>Are you sure you want to discard unsaved changes?</h2>
+						<p class="popup-button">
+							<button type="button" name="button" class="button-primary" ng-click="popup2=false;">No, take me back</button>
+						</p>
+						<p class="link-cancel">
+							<a href="#" ng-click="popup2=false;">Yes, discard changes and leave page</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+    <button type="button" name="button" class="button-default" ng-click="popup1=true">Popup with Confirmation</button>
+<div class="popup" style="display: block;" ng-show="popup1">
+	<div class="popup-mask">
+		<div class="panel">
+			<div class="panel-inner">
+				<div class="popup-cancel">
+					<a href="#" ng-click="popup1=false;"><i class="fa fa-fw fa-times"></i></a>
+				</div>
+				<h2>Thank you for your feedback!</h2>
+				<p>
+					You’ll be the first to know once the new update arrives. How exciting!
+				</p>
+				<p class="popup-button">
+					<button type="button" name="button" class="button-primary" ng-click="popup1=false;">Cool. Take me back to home page</button>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" type="text/css" href="/css/framework.css"/>
     <link href='https://fonts.googleapis.com/css?family=Rajdhani:600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">    
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+    <script type="text/javascript" src="/js/app.js"></script>
+    <script type="text/javascript" src="/js/dialog/dialog.js"></script>
 </head>
 
 <body>
