ccaed10d603b0fbb2ce3eb6854fbc74f8aee6efd
Phil Weier <pweier@my.bcit.ca>
2016-05-05 09:56:25 -0700

    start of csv import

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -5,27 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Excel;
 use App\Contact;
+use App\ContactImport;
+use Excel;
 
 class CSVController extends Controller
 {
   public function exportContactList()
+  {
+    $data = Contact::all();
+    return Excel::create('export1', function($excel) use($data) {
+      $excel->setTitle('Our new awesome title')
+      ->setCreator('Maatwebsite')
+      ->setCompany('Maatwebsite')
+      ->setDescription('A demonstration to change the file properties');
+
+      $excel->sheet('export1_sheet1', function($sheet) use($data) {
+        $sheet->fromModel($data);
+      });
+    })->export('csv');
+  }
+
+  public function importContacts()
+  {
+    Excel::filter('chunk')->load(Input::file('csvContacts'))->chunk(250, function($results)
     {
-        $data = Contact::all();
-        return Excel::create('export1', function($excel) use($data) {
-          $excel->setTitle('Our new awesome title')
-                ->setCreator('Maatwebsite')
-                ->setCompany('Maatwebsite')
-                ->setDescription('A demonstration to change the file properties');
-
-          $excel->sheet('export1_sheet1', function($sheet) use($data) {
-            $sheet->fromModel($data);
-          });
-        })->export('csv');
-    }
-
-
-
+      dd($results);
+      // foreach($results as $row)
+      // {
+      //
+      // }
+    });
+  }
 
 }
