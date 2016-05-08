c57fc6653013f365f2f02b0c1d77bb523e21d74f
jackmtr <jackmtr@hotmail.com>
2016-05-07 19:26:16 -0700

    cleaned up events page

---
+++ resources/views/eventFolder/_eventBox.blade.php
@@ -0,0 +1,20 @@
+<div class="event-box col col-md-12 col-lg-6 section-business-col">
+	<div class="box-left-side">
+		<a href="/events/{{$event['event']->event_id}}" >
+			<p>{{ date('M j, Y', strtotime($event['event']->event_date)) }}</p>
+			<p>{{$event['event']->event_name}}</p>
+			<p><span>{{$event['event']->event_location}}</span></p>
+		</a>
+	</div>
+
+	<div class="box-right-side">
+		<div class="checkedin {{$eventStatus}}">
+			<p>{{$event['count']}}</p>
+			<p>{{$eventStatusTitle}}</p>
+		</div>
+		<div class="duplicate">
+			<a href="/events/{{$event['event']->event_id}}/duplicate">Duplicate</a>
+		</div>
+	</div>
+</div>
+
--- resources/views/eventFolder/events.blade.php
+++ resources/views/eventFolder/events.blade.php
@@ -9,40 +9,11 @@
 				<h2>Active Events</h2>
 				<a href="{{ url('/events/create') }}"><i class="fa fa-plus" aria-hidden="true"></i> Add Event</a>
 			</div>
-			@forelse($eventsWithCount as $openEvent)
-				@if($openEvent['event']->event_status != 2)
-					<div class="event-box col col-md-12 col-lg-6 section-business-col">
-						<div class="box-left-side">
-							<a href="/events/{{$openEvent['event']->event_id}}" >
-								<p>{{ date('M j, Y', strtotime($openEvent['event']->event_date)) }}</p>
-								<p>{{$openEvent['event']->event_name}}</p>
-								<p><span>{{$openEvent['event']->event_location}}</span></p>
-							</a>
-						</div>
-
-						@if($openEvent['event']->event_status == 0)
-						<div class="box-right-side" style="background-color: #1e8f98">
-							<div class="checkedin">
-								<p>{{$openEvent['count']}}</p>
-								<p>Invited</p>
-							</div>
-							<div class="duplicate">
-								<a href="/events/{{$openEvent['event']->event_id}}/duplicate">Duplicate</a>
-							</div>
-						</div>
-
-						@elseif($openEvent['event']->event_status == 1)
-						<div class="box-right-side">
-							<div class="checkedin">
-								<p>{{$openEvent['count']}}</p>
-								<p>Checked in</p>
-							</div>
-							<div class="duplicate">
-								<a href="/events/{{$openEvent['event']->event_id}}/duplicate">Duplicate</a>
-							</div>
-						</div>
-						@endif
-					</div>
+			@forelse($eventsWithCount as $event)
+				@if($event['event']->event_status == 1)
+					@include('eventFolder._eventBox', ['eventStatusTitle' => 'Checked In', 'eventStatus' => 'checkedin-event'])
+				@elseif($event['event']->event_status == 0)
+					@include('eventFolder._eventBox', ['eventStatusTitle' => 'Invited', 'eventStatus' => 'open-event'])
 				@endif
 			@empty
 				<p>You do not have any current events.</p>
@@ -55,27 +26,10 @@
 			<div class="subnav">
 				<h2>Ended Events</h2>
 			</div>
-			@forelse($eventsWithCount as $closedEvent)
-			@if($closedEvent['event']->event_status == 2)
-			<div class="event-box col col-md-12 col-lg-6 section-business-col">
-				<div class="box-left-side">
-					<a class="singleeventleftbox" href="/events/{{$closedEvent['event']->event_id}}">
-						<p>{{$closedEvent['event']->event_date}}</p>
-						<p>{{$closedEvent['event']->event_name}}</p>
-						<p><span>{{$closedEvent['event']->event_location}}</span></p>
-					</a>
-				</div>
-				<div class="box-right-side" style="background-color: #eaeaea">
-					<div class="checkedin">
-						<p>{{$closedEvent['count']}}</p>
-						<p>Went</p>
-					</div>
-					<div class="duplicate">
-						<a href="/events/{{$closedEvent['event']->event_id}}/duplicate">Duplicate</a>
-					</div>
-				</div>
-			</div>
-			@endif
+			@forelse($eventsWithCount as $event)
+				@if($event['event']->event_status == 2)
+					@include('eventFolder._eventBox', ['eventStatusTitle' => 'Went', 'eventStatus' => 'completed-event'])
+				@endif
 			@empty
 				<p>You do not have any past events.</p>
 			@endforelse
