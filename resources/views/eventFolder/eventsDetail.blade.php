be3e067429e907f940e0dd7fc2b35018c53497e7
jackmtr <jackmtr@hotmail.com>
2016-05-09 11:38:00 -0700

    warning for event delete working

--- resources/views/eventFolder/eventsDetail.blade.php
+++ resources/views/eventFolder/eventsDetail.blade.php
@@ -22,9 +22,28 @@
         {!! Form::close() !!}
 
         @if( $event['event_status']  != 1)
-          {!! Form::open(['method' => 'DELETE', 'url' => 'events/' . $event->event_id, 'class' => 'form']) !!}
-            {!! Form::submit("Delete Event", ['class' => 'btn btn-primary form-control']) !!}
-          {!! Form::close() !!}
+
+          <input type="submit" name="button" class="button-default" ng-click="popupdelete = true;" value="Delete Event" />
+
+          <div class="popup ng-hide" style="display: block;" ng-show="popupdelete">
+            <div class="popup-mask">
+              <div class="panel">
+                <div class="panel-inner">
+                  <h2>Are you sure you want to delete this event?</h2>
+
+                  {!! Form::open(['method' => 'DELETE', 'url' => 'events/' . $event->event_id, 'class' => 'form']) !!}
+                    {!! Form::submit("Delete Event", ['class' => 'btn btn-primary form-control button-default']) !!}
+                  {!! Form::close() !!}                
+
+                  <p class="link-cancel">
+                    <a href="#" ng-click="popupdelete=false;">No, send me back to edits.</a>
+                  </p>
+
+                </div>
+              </div>
+            </div>
+          </div>          
+
         @endif
 
         @include('errors._list')
