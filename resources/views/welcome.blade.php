c0d5448afa3273acfbfb00a8beee1e13bea2d3a1
jackmtr <jackmtr@hotmail.com>
2016-05-12 15:10:19 -0700

    fixed conflicts with master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
       <div class="panel panel-default">
-        <div class="panel-heading">Last Updated: 5:00pm May 5,2016</div>
+        <div class="panel-heading">Last Updated: 4:00pm May 12,2016</div>
 
         <div class="panel-body">
           <h3>Team Members</h3>
